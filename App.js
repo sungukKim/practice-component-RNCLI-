2ccdf130a82ff8de7a536768b3ea73190415ec87
sung uk <sunguk0204@monkey-soft.com>
2022-02-21 19:06:55 +0900

    FirstCommit

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-import { StatusBar } from "react-native";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import App from './src/App';
 
-export default function App() {
-  const name = 'Sunguk';
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>My name is {name}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 30,
-  },
-});
+export default App;
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, View, Button } from 'react-native';
+
+const App = () => {
+    return (
+        <View
+        style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}
+        >
+            <Text style={{ fontSize: 30, marginBottom: 10 }}>Button Component</Text>
+            <Button title="Button" onPress={() => alert('click !!!')} />
+        </View>
+    );
+};
+
+export default App;
